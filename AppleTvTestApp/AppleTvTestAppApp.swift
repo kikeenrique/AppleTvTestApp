9965a5fa0289bf3a2c0d6a9cf073161f41c2892b
Enrique Garcia <github@lemambo.com>
2021-12-27 17:02:48 +0100

    Initial Commit

---
+++ AppleTvTestApp/AppleTvTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTvTestAppApp.swift
+//  AppleTvTestApp
+//
+//  Created by Enrique Garcia Alvarez on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AppleTvTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
